151adb3cd5e926182dbc18bbb7e69e11dc49f8f4
afzalhussainj <afzalhussainjatt@gmail.com>
2025-06-24 18:54:19 +0500

    models created

--- SMS/schools/models.py
+++ SMS/schools/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    branch_name = models.CharField(max_length=200)
+    branch_address = models.CharField(max_length=1000)
+    
+    def total_students(self):
+        return Student.objects.filter(branch=self).count()
+    
+    def total_staff(self):
+        return Staff.objects.filter(branch=self).count()
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    grades = ['play group', 'nursery'] + [str(i) for i in range(1,11)]
+    grade_choices = [(i,i) for i in grades]
+    grade = models.CharField(choices=grade_choices, max_length=50)
+    fee = models.IntegerField()
+    contact = models.CharField(max_length=11)
+    branch = models.ForeignKey(School, on_delete=models.PROTECT)
+
+class Staff(models.Model):
+    name = models.CharField(max_length=50)
+    designation_choices = [
+        ("principal" , "principal"),
+        ("vice principal" , "vice principal"),
+        ("teacher" , "teacher"),
+        ("incharge" , "incharge"),
+        ("peon" , "peon"),
+        ("guard" , "guard")
+    ]
+    designation = models.CharField(choices=
+                                   designation_choices, max_length=50)
+    number = models.CharField(max_length=11)
+    email = models.EmailField(max_length=250, null=True)
+    salary = models.IntegerField()
+    branch = models.ForeignKey(School, on_delete=models.PROTECT)
+    
